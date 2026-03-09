c1149f4f4e84c1d0c9f1c50dc0d2f302f646ac79
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-03-09 17:07:44 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Level order traversal/level-order-traversal.java
+++ Difficulty: Medium/Level order traversal/level-order-traversal.java
@@ -12,29 +12,22 @@ class Node {
 class Solution {
     public ArrayList<ArrayList<Integer>> levelOrder(Node root) {
         // code here
-        ArrayList<ArrayList<Integer>> res = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> ans = new ArrayList<>();
+        if(root == null) return new ArrayList<>();
         Queue<Node> q = new LinkedList<>();
         q.offer(root);
         
         while(!q.isEmpty()){
+            ArrayList<Integer> list = new ArrayList<>();
             int size = q.size();
-            ArrayList<Integer> ans = new ArrayList<>(size);
             for(int i=0;i<size;i++){
                 Node curr = q.poll();
-                ans.add(curr.data);
+                list.add(curr.data);
                 if(curr.left != null) q.offer(curr.left);
                 if(curr.right != null) q.offer(curr.right);
             }
-            res.add(ans);
+            ans.add(list);
         }
-        return res;
+        return ans;
     }
 }
-
-
-
-
-
-
-
-

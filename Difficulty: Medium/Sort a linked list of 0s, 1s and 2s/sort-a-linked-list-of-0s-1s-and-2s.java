7626a65c449456200c32447ba56b09571794c87d
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-04-22 01:07:02 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.java
+++ Difficulty: Medium/Sort a linked list of 0s, 1s and 2s/sort-a-linked-list-of-0s-1s-and-2s.java
@@ -14,39 +14,36 @@ class Solution {
     public Node segregate(Node head) {
         // code here
         if(head==null || head.next==null) return head;
-        Node temp = head;
-        int cnt0=0, cnt1=0, cnt2=0;
         
+        Node list0 = new Node(-1);
+        Node l0 = list0;
+        
+        Node list1 = new Node(-1);
+        Node l1 = list1;
+        
+        Node list2 = new Node(-1);
+        Node l2 = list2;
+        
+        Node temp = head;
         while(temp!=null){
-            if(temp.data==0) cnt0++;
-            else if(temp.data==1) cnt1++;
-            else cnt2++;
+            if(temp.data == 0) {
+                l0.next = temp;
+                l0 = l0.next;
+            }
+            else if(temp.data == 1){
+                l1.next = temp;
+                l1 = l1.next;
+            }
+            else{
+                l2.next = temp;
+                l2 = l2.next;
+            }
             temp = temp.next;
         }
-        temp = head;
-        while(cnt0-- > 0){
-            temp.data=0;
-            temp = temp.next;
-        } 
-        while(cnt1-- > 0){
-            temp.data=1;
-            temp = temp.next;
-        } 
-        while(cnt2-- > 0){
-            temp.data=2;
-            temp = temp.next;
-        } 
-        return head;
+        
+        l2.next = null;
+        l1.next = list2.next;
+        l0.next = (list1!=null) ? list1.next : list2.next;
+        return list0.next;
     }
 }
-
-
-
-
-
-
-
-
-
-
-

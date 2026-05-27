2f0b49b47c0ae591f816cc4a1a8b637f2ba55659
Deepanshi <157208739+Deepanshimittal11@users.noreply.github.com>
2026-05-27 17:15:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
+++ Difficulty: Medium/Directed Graph Cycle/directed-graph-cycle.java
@@ -1,35 +1,55 @@
 class Solution {
     public boolean isCyclic(int V, int[][] edges) {
         // code here
-        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        
+        ArrayList<ArrayList<Integer>> adjlist = new ArrayList<>();
         for(int i=0;i<V;i++){
-            adj.add(new ArrayList<>());
+            adjlist.add(new ArrayList<>());
         }
-        for(int[] e : edges){
-            adj.get(e[0]).add(e[1]);
+        for(int[] edge : edges){
+            int u = edge[0];
+            int v = edge[1];
+            adjlist.get(u).add(v);
         }
         
         int[] vis = new int[V];
         int[] pathvis = new int[V];
+        for(int i=0;i<V;i++){
+            vis[i] = 0;
+            pathvis[0] = 0; 
+        }
         
         for(int i=0;i<V;i++){
             if(vis[i]==0){
-                if(dfs(i, adj, vis, pathvis)==true) return true;   
+                if(dfs(adjlist, vis, pathvis, i, V)==true) return true;
             }
         }
         return false;
     }
-    public boolean dfs(int node, ArrayList<ArrayList<Integer>> adj, int[] vis, int[] pathvis){
-        vis[node] = 1;
-        pathvis[node] = 1;
-        for(int it : adj.get(node)){
-            if(vis[it]==0){
-                if(dfs(it, adj, vis, pathvis)==true) return true;   
+    public boolean dfs(ArrayList<ArrayList<Integer>> adjlist, int[] vis, int[] pathvis, int i, int V){
+        vis[i] = 1;
+        pathvis[i] = 1;
+        
+        for(int newnode : adjlist.get(i)){
+            if(vis[newnode]==0){
+                if(dfs(adjlist, vis, pathvis, newnode, V)==true) return true;
             }
-            else if(pathvis[it]==1) return true;
+            else if(pathvis[newnode]==1) return true;
         }
-        pathvis[node] = 0;
+        
+        pathvis[i] = 0;
         return false;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
